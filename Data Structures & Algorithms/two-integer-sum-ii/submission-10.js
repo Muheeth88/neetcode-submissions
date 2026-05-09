e29ee9d9ad5b056fc42ee2a2c71a3a73336ccbcc
Abdul Muheeth <89305708+Muheeth88@users.noreply.github.com>
2026-05-10 01:00:51 +0530

    Add: two-integer-sum-ii - submission-10

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-10.js
@@ -0,0 +1,17 @@
+class Solution {
+
+    twoSum(numbers, target) {
+        let left = 0
+        let right = numbers.length - 1
+        while(left < right) {
+            let sum = numbers[left] + numbers[right]
+            if(sum > target) {
+                right--
+            } else if(sum < target) {
+                left++
+            } else {
+                return [left + 1, right +1]
+            }
+        }
+    }
+}
