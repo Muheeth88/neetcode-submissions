b3017741215fc701a6e7e3ddb7257d45a841d2bc
Abdul Muheeth <89305708+Muheeth88@users.noreply.github.com>
2026-05-26 15:52:27 +0530

    Add: reorder-linked-list - submission-0

---
+++ Data Structures & Algorithms/reorder-linked-list/submission-0.js
@@ -0,0 +1,45 @@
+
+
+class Solution {
+    reorderList(head) {
+        if (!head || !head.next) return;
+        let slow = head;
+        let fast  = head;
+
+        while(fast && fast.next) {
+            slow = slow.next
+            fast = fast.next.next
+        }
+
+        let prev = null
+        let current = slow.next
+
+        slow.next = null;
+
+        while(current) {
+            let next = current.next
+            current.next = prev
+
+            prev = current
+            current = next
+        }
+
+
+        let first = head
+        let second = prev
+
+        while(second) {
+            let temp1 = first.next;
+            let temp2 = second.next;
+
+            first.next = second;
+            second.next = temp1;
+
+            first = temp1;
+            second = temp2;
+        }
+
+
+
+    }
+}
