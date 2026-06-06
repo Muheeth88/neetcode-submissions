20f8cc3590832d2319993d6043a7aa16ec5c8806
Abdul Muheeth <89305708+Muheeth88@users.noreply.github.com>
2026-06-07 00:17:31 +0530

    Add: combination-target-sum-ii - submission-2

---
+++ Data Structures & Algorithms/combination-target-sum-ii/submission-2.js
@@ -0,0 +1,38 @@
+class Solution {
+    /**
+     * @param {number[]} candidates
+     * @param {number} target
+     * @return {number[][]}
+     */
+    combinationSum2(candidates, target) {
+        let result = [];
+        candidates.sort((a, b) => a - b);
+
+        function backtrack(start, remaining, combination) {
+            if (remaining < 0) {
+                return;
+            }
+
+            if (remaining === 0) {
+                result.push([...combination]);
+                return;
+            }
+
+            for (let i = start; i < candidates.length; i++) {
+                if (i > start && candidates[i] === candidates[i - 1]) {
+                    continue;
+                }
+                if (candidates[i] > remaining) {
+                    break;
+                }
+                combination.push(candidates[i]);
+                backtrack(i + 1, remaining - candidates[i], combination);
+                combination.pop(candidates[i]);
+            }
+        }
+
+        backtrack(0, target, []);
+
+        return result;
+    }
+}
