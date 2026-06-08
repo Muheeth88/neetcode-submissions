5fc8611745d9b74120e59471b97e316a906a45be
Abdul Muheeth <89305708+Muheeth88@users.noreply.github.com>
2026-06-08 13:55:03 +0530

    Add: generate-parentheses - submission-2

---
+++ Data Structures & Algorithms/generate-parentheses/submission-2.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @return {string[]}
+     */
+    generateParenthesis(n) {
+        let result = []
+
+        function backtrack(open, close, current) {
+            if(current.length === 2*n) {
+                result.push(current)
+                return
+            }
+
+            if(open < n) {
+                backtrack(open + 1, close, current+"(")
+            }
+
+            if(close < open) {
+                backtrack(open, close+1, current+")")
+            }
+
+            
+
+        }
+
+        backtrack(0,0,"")
+        return result
+    }
+}
