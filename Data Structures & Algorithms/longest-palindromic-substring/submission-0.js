1ff495f1dc8a878057400b8c4c50efafbb6812db
Abdul Muheeth <89305708+Muheeth88@users.noreply.github.com>
2026-06-16 00:18:32 +0530

    Add: longest-palindromic-substring - submission-0

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-0.js
@@ -0,0 +1,42 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {string}
+     */
+    longestPalindrome(s) {
+        let res = "";
+
+        for (let i = 0; i < s.length; i++) {
+
+            let left = i;
+            let right = i;
+
+            while (left >= 0 && right < s.length && s[left] === s[right]) {
+
+                if(right - left + 1 > res.length) {
+                    res = s.slice(left, right + 1)
+                }
+
+                left--
+                right++
+            }
+
+            left = i;
+            right = i + 1;
+
+            while(left >= 0 && right < s.length && s[left] === s[right]) {
+
+                if(right - left + 1 > res.length) {
+                    res = s.slice(left, right + 1)
+                }
+
+                left--
+                right++
+            }
+        }
+
+
+
+        return res;
+    }
+}
