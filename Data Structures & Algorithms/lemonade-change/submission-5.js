b0493136c8b722be0b0c4f541bca1fad4b57b2f8
Abdul Muheeth <89305708+Muheeth88@users.noreply.github.com>
2026-06-19 03:04:35 +0530

    Add: lemonade-change - submission-5

---
+++ Data Structures & Algorithms/lemonade-change/submission-5.js
@@ -0,0 +1,43 @@
+class Solution {
+    /**
+     * @param {number[]} bills
+     * @return {boolean}
+     */
+    lemonadeChange(bills) {
+        let fives = 0
+        let tens = 0
+
+        for (let bill of bills) {
+            if(bill === 5) {
+                fives++
+                continue
+            }
+
+            if(bill === 10) {
+                if(!!fives) {
+                    fives--
+                    tens++
+                    continue
+                } else {
+                    return false
+                }
+            }
+
+            if(bill === 20) {
+                if(tens >= 1 && fives >= 1) {
+                    tens--
+                    fives--
+                    continue
+                }
+
+                if(fives >= 3) {
+                    fives -=  3
+                    continue
+                }
+                return false
+            }
+
+        }
+        return true
+    }
+}
